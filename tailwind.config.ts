debe7a9b55a5b48964993133cdbd52d51229de74
sylvaincodes <sylvaincodeur@gmail.com>
2024-03-04 21:41:56 +0100

    set ui kit - colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import  colors  from 'tailwindcss/colors'
 
 const config = {
   darkMode: ["class"],
@@ -10,11 +11,57 @@ const config = {
   ],
   prefix: "",
   theme: {
+    colors:{
+        // default colors 
+      gray: colors.gray,
+      black: '#121212',
+      white: colors.white,
+
+      // curious-blue
+      primary:{
+        50: '#f0faff',
+        100: '#e0f3fe',
+        200: '#b9e8fe',
+        300: '#7cd8fd',
+        400: '#36c5fa',
+        500: '#0caeeb',
+        600: '#008ecc',
+        700: '#016fa3',
+        800: '#065e86',
+        900: '#0b4e6f',
+        950: '#07314a',
+      }
+
+    },
+
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: '0rem',
+        sm: '12rem',
+        md: '12rem',
+        lg: '12rem',
+        xl: '12rem',
+        '2xl': '12rem',
+      },
       screens: {
-        "2xl": "1400px",
+        'xs': '320px',
+        // => @media (min-width: 320px) { ... }
+        
+        'sm': '575px',
+        // => @media (min-width: 575px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     },
     extend: {
